043ed547e5be00b0489cd6fb3bd188b87fb031e1
doandevhere <tranduongdoan03122001@gmail.com>
2024-12-04 14:36:05 +0700

    chore: optimize analyzing js bundles on build with `lucide-react`

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  optimizePackageImports: ["lucide-react"],
 };
 
 export default withBundleAnalyzer({
