8b1d9424c5c9e2b98c5fb7f934cbe3afa21a894a
doandevhere <tranduongdoan03122001@gmail.com>
2024-12-04 14:50:50 +0700

    chore: change default font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
+import clsx from "clsx";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+export const roboto_mono = Roboto_Mono({
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={clsx(roboto_mono.className, "antialiased")}>
         {children}
       </body>
     </html>
